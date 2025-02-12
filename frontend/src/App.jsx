16bd3d6eef42006e32398c38e77fe0edfda77fbe
Hamra-spec <hamrakhan2130@gmail.com>
2025-02-12 18:48:31 +0500

    hello fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,6 @@ const App = () => {
 
   return (
     <>
-      {"hello"}
       {showLogin ? <Login setshowLogin={setshowLogin} /> : <></>}
       <div className="App">
         <Navbar setshowLogin={setshowLogin}></Navbar>
