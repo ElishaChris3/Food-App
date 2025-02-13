76252b2e3963be0a687c3fa4d74ad37a6017b2aa
Rabbiya-cyber <biyafatima139@gmail.com>
2025-02-13 19:30:27 +0500

    Update StoreContext.jsx

--- frontend/src/StoreContext/StoreContext.jsx
+++ frontend/src/StoreContext/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider = (props) => {
   const [cartItem, setcartItem] = useState({});
   const [token, settoken] = useState("");
   const [food_list, setfood_list] = useState([]);
-  const url = "https://hamra-admin.vercel.app";
+  const url = "https://rabbiya-backend.vercel.app";
 
   const AddCartItems = async (itemId) => {
     if (!cartItem[itemId]) {
