11052ef45c63943ca02f268d3f6b9e8acbab4ced
Hamra-spec <hamrakhan2130@gmail.com>
2025-02-13 10:11:18 +0500

    as

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ console.log("DB URL: " + process.env.MONGO_URL);
 // ✅ Enable CORS for Specific Origin
 AppJs.use(
   cors({
-    origin: "https://admin-swart-seven.vercel.app", // Allow only frontend
+    origin: "*", // Allow only frontend
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow cookies if needed
@@ -25,10 +25,7 @@ AppJs.use(
 
 // ✅ Handle Preflight Requests (OPTIONS)
 AppJs.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://admin-swart-seven.vercel.app"
-  );
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true"); // Allow cookies if needed
